76438df2dd0099b068969ba0a70d84dd147c3633
h-vetinari <h.vetinari@gmx.com>
2025-03-13 06:47:55 +1100

    avoid picking up shared zstd library

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,4 +4,6 @@
 if [ $(uname) == Linux ]; then
   export LDSHARED="$CC -shared -pthread"
 fi
+# avoid picking up the shared library
+export CFLAGS="${CFLAGS} -DZSTD_STATIC_LINKING_ONLY -DZDICT_STATIC_LINKING_ONLY"
 ${PYTHON} setup.py install --system-zstd --single-version-externally-managed --record=record.txt
